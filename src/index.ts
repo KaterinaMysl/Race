27f364c13a9267381b1485412df5c3e5d32e39bf
KaterinaMysl <myslivchik382@gmail.com>
2023-10-20 17:15:12 +0300

    feat: add new files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import './main.css';
-import './styles/colors.css';
-import './styles/garage.css';
-import './styles/winner.css';
-import './components/api/api';
-import './components/garage/buttons';
-import './components/winner/buttons';
-import './components/main';
